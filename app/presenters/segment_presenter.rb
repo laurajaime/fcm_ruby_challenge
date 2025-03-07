ad2b430ef1cf66b41f4ab733f7c6bb1839b0b6b0
Laura Jaime <laurajaimev8@gmail.com>
2025-03-07 13:30:32 +0100

    Service refactor

--- app/presenters/segment_presenter.rb
+++ app/presenters/segment_presenter.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class SegmentPresenter
   def initialize(segment)
     @segment = segment
@@ -5,21 +7,29 @@ class SegmentPresenter
 
   attr_reader :segment
 
+  def formatted_trip_title(next_segment)
+    "TRIP to #{next_segment&.to || segment.to}"
+  end
+
+  def formatted_trip_info
+    if segment.a_hotel?
+      formatted_hotel_segment
+    elsif segment.a_transport?
+      formatted_trip_segment
+    end
+  end
+
+  private
+
+  def parse_time(time)
+    time.strftime('%H:%M')
+  end
+
   def formatted_hotel_segment
     "#{segment.segment_type} at #{segment.on} on #{segment.from_date} to #{segment.to_date}"
   end
 
   def formatted_trip_segment
     "#{segment.segment_type} from #{segment.from} to #{segment.to} at #{segment.from_date} #{parse_time(segment.from_time)} to #{parse_time(segment.to_time)}"
-  end
-
-  def formatted_trip_title(next_segment)
-    "TRIP to #{next_segment&.to || segment.to}"
-  end
-
-  private
-
-  def parse_time(time)
-    time.strftime("%H:%M")
   end
 end
--- app/services/itinerary_service.rb
+++ app/services/itinerary_service.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ItineraryService
   def initialize(based, raw_itinerary_path)
     @based = based
@@ -11,19 +13,19 @@ class ItineraryService
       existing_itinerary = Itinerary.find_by(based_iata: based, raw_content: raw_itinerary)
 
       segments = if existing_itinerary.present?
-                  existing_itinerary.segments
-                else
-                  itinerary = persist_itinerary
-                  persist_segments(itinerary)
+                   existing_itinerary.segments
+                 else
+                   itinerary = persist_itinerary
+                   persist_segments(itinerary)
 
-                  itinerary.segments
-                end
+                   itinerary.segments
+                 end
 
       # Order segments by from_date, from_time and segment_type because we want to print the itinerary in order
       # taking into account the time of the fligths or trains and hotels
 
       # Specific syntax for sqlite because of the lack of support for NULLS FIRST and NULLS LAST
-      order_time_nulls = Arel.sql("CASE WHEN from_time IS NULL THEN 1 ELSE 0 END")
+      order_time_nulls = Arel.sql('CASE WHEN from_time IS NULL THEN 1 ELSE 0 END')
       ordered_segments = segments.order(:from_date, order_time_nulls, :segment_type)
 
       print_itinerary(ordered_segments)
@@ -42,34 +44,23 @@ class ItineraryService
     segment.to == based
   end
 
-  def is_a_connection?(segment, next_segment)
-    segment.to == next_segment.from
-  end
-
-  def is_the_last_segment?(segment, next_segment)
+  def the_last_segment?(segment, next_segment)
     next_segment.nil? || segment.to == next_segment.to
   end
 
   def read_raw_itinerary(raw_itinerary_path)
     File.read(raw_itinerary_path)
   end
-  
+
   def print_itinerary(segments)
     segments.each_with_index do |segment, index|
       presenter = SegmentPresenter.new(segment)
       next_segment = index < segments.size - 1 ? segments[index + 1] : nil
 
-      if start_of_trip?(segment)
-        puts presenter.formatted_trip_title(next_segment)
-      end
+      puts presenter.formatted_trip_title(next_segment) if start_of_trip?(segment)
+      puts presenter.formatted_trip_info
 
-      if segment.is_a_hotel?
-        puts presenter.formatted_hotel_segment
-      else segment.is_a_transport?
-        puts presenter.formatted_trip_segment
-      end
-
-      puts "\n" if end_of_trip?(segment) && !is_the_last_segment?(segment, next_segment)
+      puts "\n" if end_of_trip?(segment) && !the_last_segment?(segment, next_segment)
     end
   end
 
@@ -78,10 +69,10 @@ class ItineraryService
   end
 
   def persist_segments(itinerary)
-    reservations = raw_itinerary.split("\n\n").map do |reservation|
+    raw_itinerary.split("\n\n").map do |reservation|
       reservation_info = reservation.split("\n")
       reservation_info.each do |line|
-        next unless line.start_with?("SEGMENT:")
+        next unless line.start_with?('SEGMENT:')
 
         segment_params = extract_segment(line)
         Segment.create!(segment_params.merge(itinerary: itinerary))
@@ -90,29 +81,34 @@ class ItineraryService
   end
 
   def extract_segment(line)
-    raw_segment = line.split(" ")
-    attrs = {}
+    raw_segment = line.split(' ')
 
     # That asumes that the segments has always the following format:
     # ["SEGMENT:", "Flight", "SVQ", "2023-03-02", "06:40", "->", "BCN", "09:10"]
     if raw_segment[1].in?(Segment::TRANSPORT_TYPES)
-      attrs = {
-        segment_type: raw_segment[1],
-        from: raw_segment[2],
-        from_date: raw_segment[3],
-        from_time: raw_segment[4],
-        to: raw_segment[6],
-        to_time: raw_segment[7],
-      }
+      transport_segment(raw_segment)
     else
-      attrs = {
-        segment_type: raw_segment[1],
-        on: raw_segment[2],
-        from_date: raw_segment[3],
-        to_date: raw_segment[5],
-      }
+      hotel_segment(raw_segment)
     end
-    
-    attrs
+  end
+
+  def transport_segment(raw_segment)
+    {
+      segment_type: raw_segment[1],
+      from: raw_segment[2],
+      from_date: raw_segment[3],
+      from_time: raw_segment[4],
+      to: raw_segment[6],
+      to_time: raw_segment[7]
+    }
+  end
+
+  def hotel_segment(raw_segment)
+    {
+      segment_type: raw_segment[1],
+      on: raw_segment[2],
+      from_date: raw_segment[3],
+      to_date: raw_segment[5]
+    }
   end
 end
